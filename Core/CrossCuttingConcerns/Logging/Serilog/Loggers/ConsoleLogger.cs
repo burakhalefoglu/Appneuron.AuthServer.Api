628445b1274081b21422a6c011f75ba095dde23b
appneuronco <burakhalefoglu@gmail.com>
2022-02-03 22:57:10 +0300

    refact

---
+++ Core/CrossCuttingConcerns/Logging/Serilog/Loggers/ConsoleLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Loggers
+{
+    class ConsoleLogger
+    {
+        public ConsoleLogger()
+        {
+
+            _ = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
