72f8b1aa0b9d52e1b93ddee29f2bfe4aed1dad9e
burakhalefoglu <burakhalefoglu@gmail.com>
2022-03-05 13:35:52 +0300

    Update CassandraTableQueries.cs

--- DataAccess/Concrete/Cassandra/Tables/CassandraTableQueries.cs
+++ DataAccess/Concrete/Cassandra/Tables/CassandraTableQueries.cs
@@ -10,7 +10,7 @@
         public static string Log => "CREATE TABLE IF NOT EXISTS authdatabase.logs(id bigint, message_template text, level text, time_stamp date, exception text, status boolean, PRIMARY KEY(id))";
         public static string OperationClaim => "CREATE TABLE IF NOT EXISTS authdatabase.operation_claims(id bigint, name text, alias text, description text, status boolean, PRIMARY KEY(id))";
         public static string Translate => "CREATE TABLE IF NOT EXISTS authdatabase.translates(id bigint, code text, value text, status boolean, PRIMARY KEY(id))";
-        public static string User => "CREATE TABLE IF NOT EXISTS authdatabase.users(id bigint, name text, email text, record_date date, update_contact_date date, password_salt blob, password_hash blob, reset_password_token text, reset_password_expires text, status boolean, PRIMARY KEY((email, status)))";
+        public static string User => "CREATE TABLE IF NOT EXISTS authdatabase.users(id bigint, name text, email text, record_date timestamp, update_contact_date timestamp, password_salt blob, password_hash blob, reset_password_token text, reset_password_expires text, status boolean, PRIMARY KEY((email, status)))";
         public static string UserClaim => "CREATE TABLE IF NOT EXISTS authdatabase.user_claims(id bigint, users_ıd bigint, claim_id bigint, status boolean, PRIMARY KEY(id))";
         public static string UserGroup => "CREATE TABLE IF NOT EXISTS authdatabase.user_groups(id bigint, users_ıd bigint, group_id bigint, status boolean, PRIMARY KEY(id))";
     }
