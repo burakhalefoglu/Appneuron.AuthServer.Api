f239cac66fc20c66f11305b8c8a7ccf0c01978fb
burakhalefoglu <burakhalefoglu@gmail.com>
2022-03-06 15:52:41 +0300

    Update JwtHelper.cs

--- Core/Utilities/Security/Jwt/JwtHelper.cs
+++ Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -45,7 +45,7 @@ namespace Core.Utilities.Security.Jwt
         public TAccessToken CreateCustomerToken<TAccessToken>(UserClaimModel userClaimModel, List<long> projectIdList)
             where TAccessToken : IAccessToken, new()
         {
-            _accessTokenExpiration = DateTime.Now.AddMinutes(Convert.ToDouble(_tokenOptions.AccessTokenExpiration));
+            _accessTokenExpiration = DateTime.Now.AddHours(Convert.ToDouble(_tokenOptions.AccessTokenExpiration));
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
             var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
             var jwt = CreateCustomerJwtSecurityToken(_tokenOptions, userClaimModel, signingCredentials, projectIdList);
